11bb1b6b3b875783c82c194850fe8828fcafdd8e
mohamed <mohsaidda@outlook.com>
2026-09-21 14:44:20 +0200

    Feat: initialisation du projet Spring Boot (via Spring Initializr avec H2 en dev, PostgreSQL prévu)

---
+++ src/main/java/com/eln/ElnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eln/ElnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
